995dec648c3f98d429eaeb5edadc34324a98d20e
Jessica Nguyen <jessica.stevens2115@gmail.com>
2022-03-03 16:26:28 -0700

    controllers done

--- app/controllers/api/authors_controller.rb
+++ app/controllers/api/authors_controller.rb
@@ -1,2 +1,44 @@
 class Api::AuthorsController < ApplicationController
+
+before_action :set_author, only: [:show, :update, :destroy]
+
+def index
+    render json: Author.all
+end
+
+def show
+    render json: @author
+end
+
+def create
+    @author = Author.new(authors_params)
+    if(@author.save)
+        render json: @author
+    else
+        render json: {errors: @author.errors.full_messages}, status: 422
+    end
+end
+
+def update
+    if(@author.update(authors_params))
+        render json:@author
+    else
+        render json: {errors: @author.errors.full_messages}, status: 422
+    end
+end
+
+def destroy
+    render json: @author.destroy
+end
+
+private
+
+def authors_params
+    params.require(:author).permit(:name, :age)
+end
+
+def set_author
+    @author = Author.find(params[:id])
+end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,13 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  namespace :api do
+    get 'authors', to:'authors#index'
+    get 'authors/:id', to:'authors#show'
+    post 'authors', to:'authors#create'
+    put 'authors/:id', to:'authors#update'
+    delete 'authors/:id', to:'authors#destroy'
+  end
+
 end
